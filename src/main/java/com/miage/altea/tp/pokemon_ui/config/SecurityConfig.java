5af25d004985f61839be8951cadfa332b28e3e6d
jeremyLambour <jeremy.lambour@gmail.com>
2019-04-03 00:19:21 +0200

    add support for fight view

--- src/main/java/com/miage/altea/tp/pokemon_ui/config/SecurityConfig.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.miage.altea.tp.pokemon_ui.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,25 +12,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import com.miage.altea.tp.pokemon_ui.pokemonTypes.service.TrainerService;
 import com.miage.altea.tp.pokemon_ui.pokemonTypes.service.TrainerUserDetailsService;
 
-
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	TrainerService trainerService;
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	public UserDetailsService userDetailsService() {
 		TrainerUserDetailsService details = new TrainerUserDetailsService();
 		details.setTrainerService(trainerService);
 		return details;
 	}
-	
+
 	@Autowired
 	public void setTrainerService(TrainerService service) {
 		this.trainerService = service;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.csrf().ignoringAntMatchers("/api/**");
 	}
 }
